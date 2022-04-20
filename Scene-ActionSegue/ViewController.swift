6dc2f9f462d2f65691ff479e0789aba43764864e
한규철 <hkc@hangyucheol-ui-MacBookPro.local>
2022-04-20 16:00:21 -0700

    first commit

---
+++ Scene-ActionSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scene-ActionSegue
+//
+//  Created by 한규철 on 3/21/R4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
